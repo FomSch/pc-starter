69457db1e786ede2491ae53ad2d7b411340d877a
danielfomin <upmkl@student.kit.edu>
2025-07-16 20:28:26 +0200

    added local discovery

--- webserver.js
+++ webserver.js
@@ -11,7 +11,7 @@ class WebServer {
         this.database = database;
         this.monitor = monitor;
         this.port = 3001;
-        
+
         this.setupRoutes();
         this.setupSocketIO();
     }
@@ -19,7 +19,7 @@ class WebServer {
     setupRoutes() {
         // Serve static files
         this.app.use(express.static(path.join(__dirname, 'public')));
-        
+
         // API Routes
         this.app.get('/api/temperature/:hours?', async (req, res) => {
             try {
@@ -46,7 +46,7 @@ class WebServer {
                 const temp = await this.monitor.getCPUTemp();
                 const tempStatus = this.monitor.getTempStatus(temp);
                 const systemStats = await this.monitor.getSystemStats();
-                
+
                 res.json({
                     temperature: temp,
                     status: tempStatus.status,
@@ -63,7 +63,7 @@ class WebServer {
             try {
                 const hours = parseInt(req.params.hours) || 24;
                 const csv = await this.database.exportToCSV(hours);
-                
+
                 res.setHeader('Content-Type', 'text/csv');
                 res.setHeader('Content-Disposition', `attachment; filename="temperature_data_${hours}h.csv"`);
                 res.send(csv);
@@ -85,7 +85,7 @@ class WebServer {
     setupSocketIO() {
         this.io.on('connection', (socket) => {
             console.log('Client connected to dashboard');
-            
+
             socket.on('disconnect', () => {
                 console.log('Client disconnected from dashboard');
             });
@@ -97,7 +97,7 @@ class WebServer {
             const temp = await this.monitor.getCPUTemp();
             const tempStatus = this.monitor.getTempStatus(temp);
             const systemStats = await this.monitor.getSystemStats();
-            
+
             const update = {
                 temperature: temp,
                 status: tempStatus.status,
@@ -105,7 +105,7 @@ class WebServer {
                 systemStats: systemStats,
                 timestamp: new Date().toISOString()
             };
-            
+
             this.io.emit('temperatureUpdate', update);
         } catch (error) {
             console.error('Error broadcasting update:', error.message);
@@ -113,9 +113,26 @@ class WebServer {
     }
 
     start() {
-        this.server.listen(this.port, () => {
+        this.server.listen(this.port, '0.0.0.0', () => {
             console.log(`🌐 Web dashboard running at http://localhost:${this.port}`);
             console.log(`📊 Dashboard URL: http://localhost:${this.port}/dashboard`);
+
+            // Try to get the Pi's IP address for remote access
+            const { networkInterfaces } = require('os');
+            const nets = networkInterfaces();
+            let ipAddress = 'localhost';
+
+            // Find a non-internal IPv4 address
+            for (const name of Object.keys(nets)) {
+                for (const net of nets[name]) {
+                    if (net.family === 'IPv4' && !net.internal) {
+                        ipAddress = net.address;
+                        break;
+                    }
+                }
+            }
+
+            console.log(`🌍 Remote access URL: http://${ipAddress}:${this.port}/dashboard`);
         });
 
         // Broadcast updates every 30 seconds
@@ -125,7 +142,22 @@ class WebServer {
     }
 
     getURL() {
-        return `http://localhost:${this.port}/dashboard`;
+        // Try to get the Pi's IP address for remote access
+        const { networkInterfaces } = require('os');
+        const nets = networkInterfaces();
+        let ipAddress = 'localhost';
+
+        // Find a non-internal IPv4 address
+        for (const name of Object.keys(nets)) {
+            for (const net of nets[name]) {
+                if (net.family === 'IPv4' && !net.internal) {
+                    ipAddress = net.address;
+                    break;
+                }
+            }
+        }
+
+        return `http://${ipAddress}:${this.port}/dashboard`;
     }
 }
 
